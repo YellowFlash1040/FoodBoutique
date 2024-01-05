f49728883cb50a8f264e5b314bc57b427ef475a5
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-01-05 16:37:26 +0100

    I added js functionality to the footer and also pattern attribute in the html

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,11 +1,33 @@
+import axios from "axios";
+
 const subscribe_form = document.querySelector("form.subscribe-form");
 subscribe_form.addEventListener("submit", form_Submit);
 
-function form_Submit(event)
+async function form_Submit(event)
 {
     event.preventDefault();
 
     const input = document.querySelector("input.subscribe-form-email-field");
-    alert(input.value);
+    const user_email_object =
+    {
+        email: input.value
+    }
     subscribe_form.reset();
+
+    const result = await sendEmailToBackEnd(user_email_object);
+    alert(result);
+}
+
+async function sendEmailToBackEnd(data)
+{
+    const url = "https://food-boutique.b.goit.study/api/subscription";
+    try
+    {
+        const response = await axios.post(url, data);
+        return response.data.message;
+    }
+    catch (error)
+    {
+        return error.message;
+    }
 }
