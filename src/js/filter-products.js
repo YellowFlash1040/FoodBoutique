5c8960e634f8ca21ebdbb6dfdcabba7af672ef0c
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-05 10:42:14 +0100

    requisting is better, not found is made'

--- src/js/filter-products.js
+++ src/js/filter-products.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 
+// Filters Section
 const form = document.getElementById("filters-form");
-const searchQuery = form.elements.searchQuery;
+const keywordInput = form.elements.keyword;
 const categorySelect = form.elements.category;
 // const priceAndPopularitySelect = form.elements.priceAndPopularity;
 
+// Products List Section
 const products = document.getElementById("products");
+const previousBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+const nothingFound = document.getElementById("nothing-found");
 
+// AXIOS
 axios.defaults.baseURL = "https://food-boutique.b.goit.study/api/";
 
-if (!getFilters()) {
+let totalPages = null;
+
+await fillCategories();
+
+const filters = getFilters();
+if (!filters) {
   const filters = {
     keyword: null,
     category: null,
@@ -17,45 +28,66 @@ if (!getFilters()) {
     limit: 6,
   };
   setFilters(filters);
+} else {
+  keywordInput.value = filters.keyword;
+
+  for (let i = 0; i < categorySelect.options.length; i++) {
+    if (categorySelect.options[i].value === filters.category) {
+      categorySelect.options[i].selected = true;
+      break;
+    }
+  }
 }
 
-function setFilters(filters) {
-  localStorage.setItem("filters", JSON.stringify(filters));
+await fillProductsList();
+
+// Fills products list
+async function fillProductsList() {
+  const hits = await searchProducts(getFilters());
+
+  if (hits.length > 0) {
+    hideNothingFound();
+    createCardsForProductsList(hits);
+  } else showNothingFound();
 }
 
-function getFilters() {
-  return JSON.parse(localStorage.getItem("filters"));
+async function fillCategories() {
+  const categories = await getCategories();
+  createCategories(categories);
 }
 
-// submit
-form.addEventListener("submit", search);
-//input
-form.addEventListener("change", search);
+async function getCategories() {
+  const res = await axios.get("products/categories");
+  return res.data;
+}
 
-async function search(evt) {
-  evt.preventDefault();
+function createCategories(categories) {
+  const options = [];
 
-  const query = searchQuery.value.trim();
-  const category = categorySelect.value;
-  
-  const currentFilters = getFilters();
-  
-  const filters = {
-    ...currentFilters,
-    keyword: query,
-    category,
-  };
-  setFilters(filters);
-  
+  for (const category of categories) {
+    const option = document.createElement("option");
+    option.className = "categories-option";
+    option.value = category;
+    option.innerHTML = category.replaceAll("_", " ");
+    options.push(option);
+  }
+
+  categorySelect.append(...options);
+  categorySelect.innerHTML += `
+    <option class="categories-option" value="">Show all</option>
+  `;
+}
+
+async function searchProducts(filters) {
   const res = await axios.get("products", {
-    params: filters
+    params: filters,
   });
-  
+  totalPages = res.data.totalPages;
   const hits = res.data.results;
-  createCards(hits);
+  return hits;
 }
 
-function createCards(hits) {
+function createCardsForProductsList(hits) {
   const items = [];
 
   for (const hit of hits) {
@@ -78,11 +110,11 @@ function createCards(hits) {
 
       <div class="price-and-buy-btn">
         <span class="price">$${hit.price}</span>
-        <a class="cart-link" href="./">
+        <button class="cart-btn" href="./">
           <svg class="cart-logo product">
             <use href="/images/svg/icons.svg#icon-shopping-cart"></use>
           </svg>
-        </a>
+        </button>
       </div>
     `;
 
@@ -91,4 +123,70 @@ function createCards(hits) {
 
   products.innerHTML = "";
   products.append(...items);
+}
+
+// submit
+form.addEventListener("submit", handleForm);
+// change
+form.addEventListener("input", handleForm);
+
+async function handleForm(evt) {
+  evt.preventDefault();
+
+  const keyword = keywordInput.value.trim();
+  const category = categorySelect.value;
+
+  setFilters({ ...getFilters(), keyword, category, page: 1 });
+
+  fillProductsList();
+}
+
+function setFilters(filters) {
+  localStorage.setItem("filters", JSON.stringify(filters));
+}
+
+function getFilters() {
+  return JSON.parse(localStorage.getItem("filters"));
+}
+
+previousBtn.addEventListener("click", goToPreviousPage);
+nextBtn.addEventListener("click", goToNextPage);
+
+function goToPreviousPage() {
+  const currentFilters = getFilters();
+
+  if (currentFilters.page <= 1) return;
+
+  currentFilters.page--;
+  setFilters(currentFilters);
+
+  fillProductsList();
+}
+
+function goToNextPage() {
+  const currentFilters = getFilters();
+
+  const counter = totalPages - currentFilters.page;
+  if (counter === 0) return;
+
+  currentFilters.page++;
+  setFilters(currentFilters);
+
+  fillProductsList();
+}
+
+function hideNothingFound() {
+  nothingFound.classList.add("display-none");
+
+  previousBtn.classList.remove("display-none");
+  nextBtn.classList.remove("display-none");
+}
+
+function showNothingFound() {
+  products.innerHTML = "";
+
+  nothingFound.classList.remove("display-none");
+
+  previousBtn.classList.add("display-none");
+  nextBtn.classList.add("display-none");
 }
