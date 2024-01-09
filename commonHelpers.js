ff6e16b9c49610609dcb5c21cde039c61eadc0ec
YellowFlash1040 <73052390+YellowFlash1040@users.noreply.github.com>
2024-01-09 09:44:01 +0000

    Deploying to gh-pages from @ YellowFlash1040/FoodBoutique@3bd84d5cd3add0fe721afff6d78e4cba7031e8b6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{c as m,s as d,g as y,a as g,d as f}from"./assets/footer-8a0d29db.js";import"./assets/vendor-bb737cb9.js";const a=document.getElementById("cart-items-list"),b=document.getElementById("delete-all-btn");let r=0;b.addEventListener("click",()=>{m(),d(),p(),i()});function i(){const c=document.getElementById("sum-span");c.innerText=`$${String(r)}`}async function p(){const c=y(),s=[];for(const n of c){const u=await g(n);s.push(u)}const l=document.getElementById("nothing-found-cart"),o=document.querySelector(".delete-all"),t=document.querySelector(".checkout-panel-container"),e=document.querySelector(".cart-sect");if(s.length===0){e.style.display="block",o.style.display="none",a.style.display="none",t.style.display="none",l.style.display="block";return}else e.style.display="flex",o.style.display="flex",a.style.display="block",t.style.display="block",l.style.display="none";v(s)}function v(c){const s=[];r=0;for(const t of c){r+=t.price;const e=document.createElement("li");e.className="cart-products-item";let n=`data-product-id="${t._id}"`;e.innerHTML=`
+import{c as m,s as d,g as y,a as g,i as f,d as b}from"./assets/footer-4ab1013f.js";import"./assets/vendor-bb737cb9.js";const a=document.getElementById("cart-items-list"),h=document.getElementById("delete-all-btn");let r=0;h.addEventListener("click",()=>{m(),d(),p(),i()});function i(){const c=document.getElementById("sum-span");c.innerText=`$${String(r)}`}async function p(){const c=y(),s=[];for(const n of c){const u=await g(n);s.push(u)}const l=document.getElementById("nothing-found-cart"),o=document.querySelector(".delete-all"),t=document.querySelector(".checkout-panel-container"),e=document.querySelector(".cart-sect");if(s.length===0){e.style.display="block",o.style.display="none",a.style.display="none",t.style.display="none",l.style.display="block";return}else e.style.display="flex",o.style.display="flex",a.style.display="block",t.style.display="block",l.style.display="none";v(s)}function v(c){const s=[];r=0;for(const t of c){r+=t.price;const e=document.createElement("li");e.className="cart-products-item";let n=`data-product-id="${t._id}"`;e.innerHTML=`
       <div class="cart-product-img-container">
         <img class="cart-product-img" src="${t.img}" alt="${t.name}" />
       </div>
@@ -18,8 +18,8 @@ import{c as m,s as d,g as y,a as g,d as f}from"./assets/footer-8a0d29db.js";impo
       </div>
       <button class="delete-from-cart-btn" ${n}>
         <svg class="delete-from-cart-svg">
-          <use href="/images/svg/icons.svg#icon-close"></use>
+          <use href="${f+"#icon-close"}"></use>
         </svg>
       </button>
-    `,s.push(e)}a.innerHTML="",a.append(...s);const l=document.querySelectorAll("button[data-product-id]");for(const t of l)t.addEventListener("click",o);function o(t){const n=t.currentTarget.getAttribute("data-product-id");f(n),d(),p(),i()}i()}d();p();
+    `,s.push(e)}a.innerHTML="",a.append(...s);const l=document.querySelectorAll("button[data-product-id]");for(const t of l)t.addEventListener("click",o);function o(t){const n=t.currentTarget.getAttribute("data-product-id");b(n),d(),p(),i()}i()}d();p();
 //# sourceMappingURL=commonHelpers.js.map
